8b67f2651b20d5c1087b17b9199d4417fcbfe513
waltersimekha <waltersimekha@gmail.com>
2024-03-27 10:42:51 +0300

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,90 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self.__name = name
+        self.__articles = []
+
+    @property
+    def name(self):
+        return self.__name
 
     def articles(self):
-        pass
+        return self.__articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.__articles))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self.__articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        if not self.__articles:
+            return None
+        return list(set(article.magazine.category for article in self.__articles))
+
 
 class Magazine:
+    __all_magazines = []
+
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Category must be a non-empty string")
+        self.__name = name
+        self.__category = category
+        self.__articles = []
+        Magazine.__all_magazines.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def category(self):
+        return self.__category
 
     def articles(self):
-        pass
+        return self.__articles
 
     def contributors(self):
-        pass
+        authors = [article.author for article in self.__articles]
+        return list(set(authors))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.__articles]
 
     def contributing_authors(self):
-        pass
+        authors = [article.author for article in self.__articles]
+        return [author for author in authors if authors.count(author) > 2]
+
+    @classmethod
+    def top_publisher(cls):
+        if not cls.__all_magazines:
+            return None
+        return max(cls.__all_magazines, key=lambda magazine: len(magazine.articles()))
+
+
+class Article:
+    def __init__(self, author, magazine, title):
+        self.__author = author
+        self.__magazine = magazine
+        self.__title = title
+        author.articles().append(self)
+        magazine.articles().append(self)
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def magazine(self):
+        return self.__magazine
